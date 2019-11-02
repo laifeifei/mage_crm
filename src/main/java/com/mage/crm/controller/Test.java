d0b80292f1222360610c274571d4f2715b342fc2
llf <1358991064@qq.com>
2019-11-02 14:42:03 +0800

    第三次更新

--- src/main/java/com/mage/crm/controller/Test.java
+++ src/main/java/com/mage/crm/controller/Test.java
@@ -2,6 +2,6 @@ package com.mage.crm.controller;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("你好");
+        System.out.println("你好,第二个模块");
     }
 }
